85ebce1a128881280eb27de73705425d29754cd9
GUNTHER <GUNTHER@GUNTHER>
2021-02-12 12:17:03 +0500

    ;lj

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+    }
+
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string _buttonText = string.Empty;
+
+        public string ButtonText 
+        {
+            get => _buttonText;
+            set
+            {
+                _buttonText = value;
+                InvokePropertyChanged();
+            }
+        }
+
+        private ICommand _buttonClicked = default;
+        public ICommand ButtonClicked
+        {
+            get
+            {
+                if (_buttonClicked == default)
+                    _buttonClicked = new RelayCommand((_) => 
+                    {
+                        ButtonText = _rand.Next(0, 1000).ToString();
+                    });
+                return _buttonClicked;
+            }
+        }
+
+
+        private ObservableCollection<ListViewItemModel> _itemsCollection = new ObservableCollection<ListViewItemModel>();
+        public ObservableCollection<ListViewItemModel> ItemsCollection 
+        {
+            get => _itemsCollection;
+            set
+            {
+                _itemsCollection = value;
+                InvokePropertyChanged();
+            }
+        }
+
+        private string _entryText = string.Empty;
+        public string EntryText
+        {
+            get => _entryText;
+            set
+            {
+                _entryText = value;
+                InvokePropertyChanged();
+            }
+        }
+
+        private ICommand _addEntityCommand = default;
+        public ICommand AddEntityCommand
+        {
+            get
+            {
+                return _addEntityCommand ?? (_addEntityCommand = new RelayCommand((_)=> 
+                {
+                    if (ItemsCollection.FirstOrDefault(m => m.StringField == EntryText) == default)
+                        ItemsCollection.Insert(0, new ListViewItemModel() { Id = _rand.Next(0, 10000), StringField = EntryText });
+                }));
+            }
+        }
+
+        private Random _rand = new Random();
+    }
+
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, (_) => true) { }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+    }
+
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+        protected void InvokePropertyChanged([CallerMemberName] string memberName = "")
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(memberName));
+        }
+    }
+
+    public class ListViewItemModel : ViewModelBase
+    {
+        public int Id { get; set; }
+        public string StringField { get; set; }
+
+
+    }
+
+}
---
+++ AppTestServer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AppTestServer
+{
+    class Program
+    {
+        private static EventWaitHandle _waitHandler = new EventWaitHandle(false, EventResetMode.ManualReset);
+
+        static async Task Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Any, 9091));
+            listener.Start();
+            _ = _AcceptTcpClient(listener);
+
+            Console.WriteLine("Ожидание подключения....");
+            _waitHandler.WaitOne();
+            Console.WriteLine("Клиент подключен");
+            
+            while (true)
+            {
+                string ln = Console.ReadLine();
+                StreamWriter sw = new StreamWriter(_tcpClient.GetStream());
+                sw.WriteLine(ln);
+            }
+        }
+
+        static async Task _AcceptTcpClient(TcpListener listener)
+        {
+            await Task.Delay(1);
+            while (true)
+            {
+                TcpClient tcpClient = listener.AcceptTcpClient();
+                _ = _ProcessTcpClient(tcpClient);
+                await Task.Delay(100);
+            }
+        }
+
+        private static TcpClient _tcpClient;
+
+        static async Task _ProcessTcpClient(TcpClient tcp)
+        {
+            _tcpClient = tcp;
+            await Task.Delay(10);
+            NetworkStream netStream = tcp.GetStream();
+            StreamReader reader = new StreamReader(netStream);
+            while (true)
+            {
+                if (netStream.DataAvailable)
+                {
+                    string dataString = reader.ReadLine();
+                    Console.WriteLine($"[CLIENT] {dataString}");
+                }
+
+                await Task.Delay(100);
+            }
+        }
+
+
+        
+    }
+}
